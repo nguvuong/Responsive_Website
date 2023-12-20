08b5cebf6d2fcbf57cd1da64ad742e5ce0fa30f1
Vuong Quoc Nguyen <nguvuong@sheridancollege.ca>
2023-12-20 02:22:20 -0500

    completed designing the topics section

--- front-end/src/assets/index.js
+++ front-end/src/assets/index.js
@@ -1,3 +1,10 @@
 import heroimage from "./hero-image.png"
+import DAW from "./DAW.png"
+import frequencies from "./frequencies.png"
+import mastering from "./Mastering.png"
+import mixing from "./Mixing.png"
+import mixingconsole from "./MixingConsole.png"
+import vocalsprocessing from "./VocalsProcessing.png"
+import bgelement1 from "./bg-element1.svg"
 
-export { heroimage };
+export { heroimage, DAW, frequencies, mastering, mixing, mixingconsole, vocalsprocessing, bgelement1 };
--- front-end/src/components/Topics/Topics.jsx
+++ front-end/src/components/Topics/Topics.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Topics.css"
+import { DAW, frequencies, mastering, mixing, mixingconsole, vocalsprocessing, bgelement1 } from "../../assets"
 
 const Topics = () => {
+    const [currentImage, setCurrentImage] = useState(frequencies);
+
+
+
     return (
-        <div>Topics</div>
+        <section id='topics' className='black'>
+            <div className='wrapper'>
+                <h2>What will you learn?</h2>
+                <div className='content-container'>
+                    <ul className='topics-list'>
+                        <li onMouseEnter={() => setCurrentImage(frequencies)}>What are frequencies?</li>
+                        <li onMouseEnter={() => setCurrentImage(DAW)}>Using DAW</li>
+                        <li onMouseEnter={() => setCurrentImage(vocalsprocessing)}>Vocals Processing</li>
+                        <li onMouseEnter={() => setCurrentImage(mixing)}>Mixing</li>
+                        <li onMouseEnter={() => setCurrentImage(mixingconsole)}>Mixing Console</li>
+                        <li onMouseEnter={() => setCurrentImage(mastering)}>Mastering</li>
+                    </ul>
+                    <div className='topic-image'>
+                        <img src={currentImage} alt="" />
+                    </div>
+                </div>
+                <img src={bgelement1} className='bg-element-1' />
+
+            </div>
+
+        </section>
     )
 }
 
