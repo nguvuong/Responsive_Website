a9c5a4f7a3c1c626736eb8eab89ffcbac950830d
Vuong Quoc Nguyen <nguvuong@sheridancollege.ca>
2023-12-19 23:56:52 -0500

    Created Components

---
+++ front-end/src/components/index.js
@@ -0,0 +1,8 @@
+import Header from "./Header/Header";
+import Blog from "./Blog/Blog";
+import Footer from "./Footer/Footer";
+import Info from "./Info/Info";
+import Testimonials from "./Testimonials/Testimonials";
+import Topics from "./Topics/Topics";
+
+export {Header, Topics, Blog, Footer, Info, Testimonials}
