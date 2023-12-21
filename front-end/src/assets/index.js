89fbe728b7de0ce04e233e36f37c9255be560a03
Vuong Quoc Nguyen <nguvuong@sheridancollege.ca>
2023-12-20 23:58:25 -0500

    finished designing the posts section

--- front-end/src/assets/index.js
+++ front-end/src/assets/index.js
@@ -9,5 +9,12 @@ import bgelement1 from "./bg-element1.svg"
 import studenticon from "./studenticon.svg"
 import videoicon from "./videoicon.svg"
 import bgelement2 from "./bg-element2.svg"
+import blog1 from "./Blog1.png"
+import blog2 from "./Blog2.png"
+import blog3 from "./Blog3.png"
 
-export { heroimage, DAW, frequencies, mastering, mixing, mixingconsole, vocalsprocessing, bgelement1, videoicon, studenticon, bgelement2 };
+
+export { heroimage, DAW, frequencies, mastering, mixing, mixingconsole, vocalsprocessing, bgelement1, videoicon, studenticon, bgelement2
+, blog1
+, blog2
+, blog3 };
--- front-end/src/components/Blog/Blog.jsx
+++ front-end/src/components/Blog/Blog.jsx
@@ -1,12 +1,52 @@
 import React from 'react';
 import "./Blog.css";
+import { blog1, blog2, blog3 } from '../../assets';
 
 
 
 /// this is basic code for react components
 const Blog = () => {
     return (
-        <div>Blog</div>
+        <section id='blog' className='pink'>
+            <div className='wrapper'>
+                <h2 className='light'>Lastest Posts</h2>
+                <div className='content-container'>
+                    <div className='post'>
+                        <div className='tag'>DAW</div>
+                        <a href="#">
+                            <img src={blog1} alt="" />
+                        </a>
+                        <a href="">
+                            <h3 className='post-title'>How to Use Drum Machine in Logic Pro X</h3>
+                        </a>
+                    </div>
+                    <div className='post'>
+                        <div className='tag'>Mixing</div>
+                        <a href="#">
+                            <img src={blog2} alt="" />
+                        </a>
+                        <a href="">
+                            <h3 className='post-title'>How to Mix Guitars Effectively</h3>
+                        </a>
+                    </div>
+                    <div className='post'>
+                        <div className='tag'>Vox</div>
+                        <a href="#">
+                            <img src={blog3} alt="" />
+                        </a>
+                        <a href="">
+                            <h3 className='post-title'>The Real Power of Harmonies in Music Production</h3>
+                        </a>
+                    </div>
+                </div>
+
+                <div className='btn-container'>
+                    <a href="#" className='all-posts-btn'>All Posts</a>
+                </div>
+
+            </div>
+
+        </section>
     )
 }
 
