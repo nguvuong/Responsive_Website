0c69b7169c0dca312bd2293705ff9353adbc5f1f
Vuong Quoc Nguyen <nguvuong@sheridancollege.ca>
2023-12-20 23:08:28 -0500

    finished designing the info section

--- front-end/src/assets/index.js
+++ front-end/src/assets/index.js
@@ -6,5 +6,8 @@ import mixing from "./Mixing.png"
 import mixingconsole from "./MixingConsole.png"
 import vocalsprocessing from "./VocalsProcessing.png"
 import bgelement1 from "./bg-element1.svg"
+import studenticon from "./studenticon.svg"
+import videoicon from "./videoicon.svg"
+import bgelement2 from "./bg-element2.svg"
 
-export { heroimage, DAW, frequencies, mastering, mixing, mixingconsole, vocalsprocessing, bgelement1 };
+export { heroimage, DAW, frequencies, mastering, mixing, mixingconsole, vocalsprocessing, bgelement1, videoicon, studenticon, bgelement2 };
--- front-end/src/components/Info/Info.jsx
+++ front-end/src/components/Info/Info.jsx
@@ -1,9 +1,38 @@
 import React from 'react'
 import "./Info.css"
+import { studenticon, videoicon, bgelement2 } from '../../assets'
 
 const Info = () => {
+
+    // clasName with color to set the background of this section
     return (
-        <div>Info</div>
+        <section id='info' className='dark-grey'>
+
+            {/* wrapper class to have the fix width */}
+            <div className='wrapper'>
+
+                {/* flex attribute is in this class,  so that the components inside this tag will one next to each other */}
+                <div className='content-container'>
+                    <div className='info-content'>
+                        <img src={studenticon} alt="" />
+                        <div className='amount'>23,000+</div>
+                        <div className='type'>Students</div>
+                    </div>
+
+                    <div className='info-content'>
+                        <img src={videoicon} alt="" />
+                        <div className='amount'>26 Hrs</div>
+                        <div className='type'>Video Content</div>
+                    </div>
+
+
+                </div>
+
+                <img className='bg-element-2' src={bgelement2} alt="" />
+
+            </div>
+
+        </section>
     )
 }
 
