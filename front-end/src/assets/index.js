6c8538c8b308e61af8712eaa4a10f40c22871f2d
Vuong Quoc Nguyen <nguvuong@sheridancollege.ca>
2023-12-21 00:49:24 -0500

    finish designing the footer

--- front-end/src/assets/index.js
+++ front-end/src/assets/index.js
@@ -12,9 +12,22 @@ import bgelement2 from "./bg-element2.svg"
 import blog1 from "./Blog1.png"
 import blog2 from "./Blog2.png"
 import blog3 from "./Blog3.png"
+import testimonial1 from "./Testimonials1.png"
+import testimonial2 from "./Testimonials2.png"
+import testimonial3 from "./Testimonials3.png"
+import facebook from "./facebook.svg"
+import twitter from "./twitter.svg"
+import youtube from "./youtube.svg"
+
 
 
 export { heroimage, DAW, frequencies, mastering, mixing, mixingconsole, vocalsprocessing, bgelement1, videoicon, studenticon, bgelement2
 , blog1
 , blog2
-, blog3 };
+, blog3 
+, testimonial1
+, testimonial2
+, testimonial3
+, youtube
+, facebook 
+, twitter};
--- front-end/src/components/Footer/Footer.jsx
+++ front-end/src/components/Footer/Footer.jsx
@@ -1,9 +1,56 @@
 import React from 'react'
 import "./Footer.css"
+import { facebook, youtube, twitter } from '../../assets'
 
 const Footer = () => {
     return (
-        <div>Footer</div>
+        <footer className='black'>
+            <div className='wrapper'>
+                <div className='content-container'>
+                    <div className='links'>
+                        <a href="#" className='logo'>
+                            Sound<span className='red'>DZign</span>
+                        </a>
+                        <div className='social-icons'>
+                            <a href="">
+                                <img src={facebook} alt="" />
+                            </a>
+                            <a href="">
+                                <img src={youtube} alt="" />
+                            </a>
+                            <a href="">
+                                <img src={twitter} alt="" />
+                            </a>
+                        </div>
+                        <div className='copyright'>
+                            This website is designed by Vuong & GTCoding  © 2023
+                        </div>
+                    </div>
+                    <div className='links'>
+                        <h3>Quick Links</h3>
+                        <ul>
+                            <li>
+                                <a href="#">Blog</a>
+                            </li>
+                            <li>
+                                <a href="#">Privacy Policy</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className='links'>
+                        <h3>Contact Us</h3>
+                        <ul>
+                            <li>
+                                <a href="#">nguyenvuongquoc8@gmail.com</a>
+                            </li>
+                            <li>
+                                <a href="#">+1 999 999 9999</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </footer>
     )
 }
 
